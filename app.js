9e59d47033d4a43d2e0c37a22126f809fcffa7e8
surya <thakursuryapratap9@gmail.com>
2023-05-17 22:26:06 +0530

    updated

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config()
 // connect mongodb
-const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}0@${process.env.DB_NAME}.sbunmxg.mongodb.net/?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.sbunmxg.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 app.set('view engine', 'ejs');
 
 
-app.listen(3000, function () {
-    console.log('listening on port 3000');
-});
+try {
+    mongoose.connect(dbURI).then((res) => {
+        console.log("DB connected");
+        app.listen(3000, function () {
+            console.log('listening on port 3000');
+        });
+    });
+} catch (err) {
+    console.log(err);
+}
+
+
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Running...</h1>');
-    // res.sendFile('./views/index.html', { root: __dirname })
     res.render('index', { title: 'Index' });
 
 })
 app.get('/about', (req, res) => {
-    // res.sendFile('./views/about.html', { root: __dirname })
+
     res.render('about', { title: 'About' });
 })
 app.get('/create', (req, res) => {
-    // res.sendFile('./views/about.html', { root: __dirname })
     res.render('createblog', { title: 'CreateBlog' });
 })
 app.get('/aboutus', (req, res) => {
